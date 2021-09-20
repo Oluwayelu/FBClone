cd159c17af8f45c470ce41c96efa81a66caaf1a8
oluwayelu <oluwayeluifeoluwa@gmail.com>
2021-09-20 16:37:23 -0700

    updated facebook clone

--- components/InputBox.jsx
+++ components/InputBox.jsx
@@ -1,19 +1,20 @@
-import { useRef, useState } from "react";
-import firebase from "firebase/app";
-import { useSession } from "next-auth/client";
-import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+import { useSession } from "next-auth/client";
+import { useRef, useState } from "react";
 import { db, storage } from "../firebase";
+import firebase from "firebase";
+import Image from "next/image";
 
-const InputBox = () => {
+function InputBox() {
   const [session] = useSession();
   const inputRef = useRef(null);
-  const filePickerRef = useRef(null);
   const [imageToPost, setImageToPost] = useState(null);
+  const filepickerRef = useRef(null);
 
   const sendPost = (e) => {
     e.preventDefault();
+
     if (!inputRef.current.value) return;
 
     db.collection("posts")
@@ -33,10 +34,14 @@ const InputBox = () => {
           removeImage();
 
           uploadTask.on(
-            "state_change",
+            "state_changed",
             null,
-            (error) => console.error(error),
+            (error) => {
+              // ERROR function
+              console.log(error);
+            },
             () => {
+              // COMPLETE function
               storage
                 .ref("posts")
                 .child(doc.id)
@@ -62,6 +67,7 @@ const InputBox = () => {
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
+
     reader.onload = (readerEvent) => {
       setImageToPost(readerEvent.target.result);
     };
@@ -70,6 +76,7 @@ const InputBox = () => {
   const removeImage = () => {
     setImageToPost(null);
   };
+
   return (
     <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
       <div className="flex space-x-4 p-4 items-center">
@@ -79,16 +86,15 @@ const InputBox = () => {
           width={40}
           height={40}
           layout="fixed"
-          alt="profile"
         />
-        <form action="" className="flex flex-1">
+        <form className="flex flex-1">
           <input
-            type="text"
-            ref={inputRef}
             className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
-            placeholder={`What's on your mind ${session.user.name}`}
+            type="text"
+            placeholder={`What's on your mind, ${session.user.name}?`}
+            ref={inputRef}
           />
-          <button hidden type="submit" onClick={sendPost}>
+          <button hidden onClick={sendPost}>
             Submit
           </button>
         </form>
@@ -98,7 +104,7 @@ const InputBox = () => {
             onClick={removeImage}
             className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
           >
-            <img src={imageToPost} alt="" className="h-18 object-contain" />
+            <img className="h-10 object-contain " src={imageToPost} alt="" />
             <p className="text-xs text-red-500 text-center">Remove</p>
           </div>
         )}
@@ -109,19 +115,21 @@ const InputBox = () => {
           <VideoCameraIcon className="h-7 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
+
         <div
-          onClick={() => filePickerRef.current.click()}
+          onClick={() => filepickerRef.current.click()}
           className="inputIcon"
         >
           <CameraIcon className="h-7 text-green-400" />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
           <input
-            ref={filePickerRef}
             onChange={addImageToPost}
+            ref={filepickerRef}
             type="file"
             hidden
           />
         </div>
+
         <div className="inputIcon">
           <EmojiHappyIcon className="h-7 text-yellow-300" />
           <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity</p>
@@ -129,6 +137,6 @@ const InputBox = () => {
       </div>
     </div>
   );
-};
+}
 
 export default InputBox;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,45 @@
 import { getSession } from "next-auth/client";
 import Head from "next/head";
-import Login from "../components/Login";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
+import Header from "../components/Header";
+import Login from "../components/Login";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import { db } from "../firebase";
 
-export default function Home({ session }) {
+export default function Home({ session, posts }) {
   if (!session) return <Login />;
+
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook</title>
       </Head>
+
       <Header />
+
       <main className="flex">
         <Sidebar />
-        <Feed />
+        <Feed posts={posts} />
+        <Widgets />
       </main>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  //Get the user
+  // Get User
   const session = await getSession(context);
 
+  const posts = await db.collection("posts").orderBy("timestamp", "desc").get();
+
+  const docs = posts.docs.map((post) => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null,
+  }));
+
   return {
-    props: {
-      session,
-    },
+    props: { session, posts: docs },
   };
 }
